74318ded11380c3534605500702d75a221e97f95
JayaSuhane <suhane79jaya@gmail.com>
2023-12-18 08:25:07 +0530

    project with profile functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
 //import DemoComponent from "./DemoComponent";
-import Home from "./components/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import Home from "./components/Home/Home";
+import ResumeListing from "./pages/ResumeListing";
+import ResumeCreate from "./pages/ResumeCreate";
+import ResumeDetails from "./pages/ResumeDetails";
+import ResumeEdit from "./pages/ResumeEdit";
 function App() {
   return (
     <div className="App">
-      <Home />
-      {/* <DemoComponent /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ResumeListing />}></Route>
+          <Route
+            path="/resumeProfile/create"
+            element={<ResumeCreate />}
+          ></Route>
+          <Route
+            path="/resumeProfile/edit/:id"
+            element={<ResumeEdit />}
+          ></Route>
+          <Route
+            path="/resumeProfile/details/:id"
+            element={<ResumeDetails />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
